d5b906abcf6d9ba2dcea101f1c32911f0282c20b
GordonChen19 <chengordon8@gmail.com>
2023-03-20 21:40:44 +0800

    First commit

---
+++ Website/models.py
@@ -0,0 +1,53 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class RIASEC_Scores(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    r_score=db.Column(db.Integer)
+    i_score=db.Column(db.Integer)
+    a_score=db.Column(db.Integer)
+    s_score=db.Column(db.Integer)
+    e_score=db.Column(db.Integer)
+    c_score=db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    qualification=db.relationship('Qualification')
+    subject_interests=db.relationship('Subject_interests')
+    riasec_scores=db.relationship('RIASEC_Scores')
+
+class Qualification(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    curriculum=db.Column(db.String(150))
+    alevel_score=db.Column(db.String(50))
+    ib_score=db.Column(db.Integer)
+    polytechnic_score=db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Subject_interests(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    subject1=db.Column(db.String(50))
+    subject2=db.Column(db.String(50))
+    subject3=db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class IGP(db.Model):
+    school=db.Column(db.String(150),primary_key=True)
+    degree=db.Column(db.String(150),primary_key=True)
+    alevel_igp=db.Column(db.String(150))
+    polytechnic_igp=db.Column(db.Float)
+    
+
