fe0e468806b4ccd10593c2ae3edf4663aab3c6f6
GordonChen19 <chengordon8@gmail.com>
2023-03-21 21:37:20 +0800

    Merge

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for,redirect
 from flask_login import login_required, current_user
 from .models import *
 from . import db
@@ -71,5 +71,31 @@ def add_portfolio():
     
     return render_template("academic_portfolio.html", user=current_user)   
             
-        
+@views.route('/TakeTest', methods=['POST', 'GET'])
+@login_required
+def receive_input():
+    if request.method=='POST':
+        r_score,i_score,a_score,s_score,e_score,c_score=0,0,0,0,0,0
+        number_of_questions=42
+        for i in range(number_of_questions):
+            response=request.form.get('q'+str(i))
+            if response=='r':
+                r_score+=1
+            elif response=='i':
+                i_score+=1
+            elif response=='a':
+                a_score+=1
+            elif response=='s':
+                s_score+=1
+            elif response=='e':
+                e_score+=1
+            elif response=='c':
+                c_score+=1
+
+        riasec_scores=RIASEC_Scores(r_score=r_score,i_score=i_score,a_score=a_score,s_score=s_score,e_score=e_score,c_score=c_score,user_id=current_user.id)
+        db.session.add(riasec_scores)
+        db.session.commit()
+        return redirect(url_for('views.add_info'))
+
+    return render_template("take_test.html", user=current_user)
 
